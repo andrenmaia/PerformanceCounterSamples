31ae80e7059ba2ef32f036a6bf2e2188a5ede336
Andre Maia <andrenmaia@gmail.com>
2014-02-26 03:16:43 -0300

    Exemplo de performance counter.

---
+++ Counters/PerformanceCounterCategoriCreator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Counters
+{
+    class PerformanceCounterCategoriCreator
+    {
+       public const string NOME_CATEGORIA = "CountersCategory";
+
+        internal void Create()
+        {
+            PerformanceCounterCategory.Delete(NOME_CATEGORIA);
+
+            // 1. Verifica se a categoria já existe
+            if (!PerformanceCounterCategory.Exists(NOME_CATEGORIA))
+            {
+
+                // 2. Cria a coleção de contadores e a preenche.
+                var counterCollection = new CounterCreationDataCollection();
+
+                // Contador - Média
+                var media = GetNewCounterCreationData("Tempo médio por operação", PerformanceCounterType.AverageCount64);
+                counterCollection.Add(media);
+
+                var mediaBase = GetNewCounterCreationData("Tempo médio por operação BASE", PerformanceCounterType.AverageBase);
+                counterCollection.Add(mediaBase);
+
+                // Contador - Diferença/Variação 
+                var diferenca = GetNewCounterCreationData("Diferença/Variação", PerformanceCounterType.CounterDelta32);
+                counterCollection.Add(diferenca);
+
+                // Contador - Instantânea
+                var instantanea = GetNewCounterCreationData("Número de operações executadas", PerformanceCounterType.NumberOfItems32);
+                counterCollection.Add(instantanea);
+
+                // Contador - Porcentagem
+                var porcentagem = GetNewCounterCreationData("Porcentagem", PerformanceCounterType.CounterTimer);
+                counterCollection.Add(porcentagem);
+
+                // Contador - Porcentagem
+                var taxa = GetNewCounterCreationData("Número de operações por seg", PerformanceCounterType.RateOfCountsPerSecond32);
+                counterCollection.Add(taxa);
+
+                // 3. Cria a categoria do performance counter.
+                PerformanceCounterCategory.Create(
+                    NOME_CATEGORIA,
+                    "categoria para exemplo de contadores",
+                    PerformanceCounterCategoryType.SingleInstance,
+                    counterCollection
+                );
+            }
+        }
+
+        private CounterCreationData GetNewCounterCreationData(string name, PerformanceCounterType performanceCounterType)
+        {
+            var counter = new CounterCreationData();
+            counter.CounterName = name;
+            counter.CounterHelp = "Contador  - " + name;
+            counter.CounterType = performanceCounterType;
+
+            return counter;
+        }
+    }
+}
---
+++ Counters/PerformanceCounterIncrementer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counters
+{
+    class PerformanceCounterIncrementer
+    {
+        [DllImport("Kernel32.dll")]
+        public static extern void QueryPerformanceCounter(ref long ticks);
+
+        internal void Start()
+        {
+            // Cria as instâncias dos contadores para atualizar seus valores.
+            var media = GetPerfCounter("Tempo médio por operação");
+            var mediaBase = GetPerfCounter("Tempo médio por operação BASE");
+            var diferenca = GetPerfCounter("Diferença/Variação");
+            var instantanea = GetPerfCounter("Número de operações executadas");
+            var porcentagem = GetPerfCounter("Porcentagem");
+            var taxa = GetPerfCounter("Número de operações por seg");
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    var tick = GetTickForPerfCounter();
+
+                    // Se o tick for par, incrementa, caso contrário, decrementa
+                    //if (tick % 2 == 0)
+                    //{
+                    media.IncrementBy(tick);
+                    mediaBase.Increment();
+                    diferenca.Increment();
+                    instantanea.IncrementBy(5);
+                    porcentagem.Increment();
+                    taxa.Increment();
+                    //}
+                    //else
+                    //{
+                    //    media.IncrementBy(tick);
+                    //    mediaBase.Decrement();
+                    //    diferenca.Decrement();
+                    //    instantanea.Decrement();
+                    //    porcentagem.Decrement();
+                    //    taxa.Decrement();
+                    //}
+
+
+                    Console.WriteLine("Atualizou o valor dos contadores.");
+                    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+                }
+            });
+
+        }
+
+        private long GetTickForPerfCounter()
+        {
+
+            /// Este método é utilizado porque  a
+            /// System.DateTime.Now.Ticks não é preciso o suficiente para o 
+            /// calculo da média.
+            var rand = new Random();
+            var startTime = 0L;
+            var endTime = 0L;
+
+            // mede tempo inicial.
+            QueryPerformanceCounter(ref startTime);
+
+            System.Threading.Thread.Sleep(rand.Next(500));
+
+            // mede tempo final.
+            QueryPerformanceCounter(ref endTime);
+
+            // Tick = variação de tempo.
+            return endTime - startTime; ;
+        }
+
+        private PerformanceCounter GetPerfCounter(string name)
+        {
+            var _perfCounter = new PerformanceCounter();
+            _perfCounter.CategoryName = PerformanceCounterCategoriCreator.NOME_CATEGORIA;
+            _perfCounter.CounterName = name;
+            _perfCounter.MachineName = ".";
+            _perfCounter.ReadOnly = false;
+
+            return _perfCounter;
+        }
+    }
+}
